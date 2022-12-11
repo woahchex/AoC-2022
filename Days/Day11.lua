0013a815da08b9b9adc351974f3171f6b4351f93
woahchex <95668604+woahchex@users.noreply.github.com>
2022-12-11 11:09:24 -0500

    shortened day11

--- Days/Day11.lua
+++ Days/Day11.lua
@@ -1,19 +1,20 @@
 local Day11 Day11 = setmetatable({
+  split = Advent.Days[10].split,
   ops = {
     ["+"] = function(a, b) return a+b end, ["-"] = function(a, b) return a-b end,
     ["*"] = function(a, b) return a*b end, ["/"] = function(a, b) return a/b end,
     ["Monkey"] = function(l, i, ln, s)  Day11.cMonkey = tonumber(s[2]:sub(1, #s[2]-1)) + 1  end,
-    ["Starting"] = function(l, i, ln, s)  l[i] = Advent.Days[10].split(Advent.Days[10].split(ln, ":")[2], ", ") end,
-    ["Operation:"] = function(l, i, ln, s)  l[i].operation = Advent.Days[10].split(Advent.Days[10].split(ln, ":")[2], " ")  end,
+    ["Starting"] = function(l, i, ln, s)  l[i] = Day11.split(Day11.split(ln, ":")[2], ", ") end,
+    ["Operation:"] = function(l, i, ln, s)  l[i].op = Day11.split(Day11.split(ln, ":")[2], " ")  end,
     ["Test:"] = function(l, i, ln, s)  l[i].testDivide = s[#s]; Day11.product = Day11.product * l[i].testDivide  end,
-    ["If"] = function(l, i, ln, s)  l[i].operation[s[2]:sub(1,#s[2]-1)] = s[#s]  end,
+    ["If"] = function(l, i, ln, s)  l[i].op[s[2]:sub(1,#s[2]-1)] = s[#s]  end,
     [""] = function() end
   },
   solve = function(input, rounds, worryDivisor)
     local monkys = {}
     Day11.product, Day11.cMonkey = 1, 0
-    for _, line in ipairs(Advent.Days[10].split(input)) do
-      local split = Advent.Days[10].split(line, "%s")
+    for _, line in ipairs(Day11.split(input)) do
+      local split = Day11.split(line, "%s")
       Day11.ops[split[1]](monkys, Day11.cMonkey, line, split)
     end
     
@@ -21,13 +22,12 @@ local Day11 Day11 = setmetatable({
       for monkeyIndex, monkey in ipairs(monkys) do
         for item, worry in ipairs(monkey) do
           monkey.moves = (monkey.moves or 0) + 1
-          monkey[item] = math.floor(Day11.ops[monkey.operation[4]](
-            monkey.operation[3]=="old" and worry or monkey.operation[3], 
-            monkey.operation[5]=="old" and worry or monkey.operation[5]
+          monkey[item] = math.floor(Day11.ops[monkey.op[4]](
+            monkey.op[3]=="old" and worry or monkey.op[3], 
+            monkey.op[5]=="old" and worry or monkey.op[5]
           ) / worryDivisor) % Day11.product
           
-          local truth = tostring(math.floor(monkey[item]/monkey.testDivide) == monkey[item]/monkey.testDivide)
-          table.insert(monkys[monkey.operation[truth]+1], monkey[item])
+          table.insert(monkys[monkey.op[tostring(monkey[item]%monkey.testDivide==0)]+1], monkey[item])
         end
         for i = 1, #monkey do
           monkey[i]=nil
